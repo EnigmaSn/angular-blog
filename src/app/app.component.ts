b2bbee4f1478f321594f35e01a9cdb996ef4f57c
Anna Ponomareva <aponomareva@luxoft.com>
2020-07-15 22:51:02 +0300

    add components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,48 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  // title = 'Initial';
-  // onInput(event: any) {
-  //   this.title = event.target.value;
-  // }
 
-  //backgroundToggle = false;
-
-  // toggle:any = false;
-
-  // arr = [1, 1, 2, 3, 5, 8, 13]
-  // posts = [
-  //   {
-  //     title: 'Post 1',
-  //     author: 'Anna',
-  //     comments: [
-  //       {
-  //         name: 'Alan',
-  //         text: 'lorem5'
-  //       },
-  //       {
-  //         name: 'Daniel',
-  //         text: 'lorem10'
-  //       }
-  //
-  //     ]
-  //   },
-  //   {
-  //     title: 'Post 2',
-  //     author: 'Anna 2',
-  //     comments: [
-  //       {
-  //         name: 'Alan 2',
-  //         text: 'lorem5'
-  //       },
-  //       {
-  //         name: 'Daniel 2',
-  //         text: 'lorem10'
-  //       }
-  //
-  //     ]
-  //   }
-  // ]
-
-  now: Date = new Date();
 }
